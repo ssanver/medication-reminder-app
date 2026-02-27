946bcc780cc538c3a2f4de329a4cf4f2f8c1f14e
k.ss200061 <k.ss200061@192.168.1.9>
2026-02-28 01:30:58 +0300

    chore db: cleanup legacy preference-only seed account

---
+++ api/data/migrations/20260227222637_CleanupPreferenceOnlySeedAccount.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.data.migrations
+{
+    /// <inheritdoc />
+    public partial class CleanupPreferenceOnlySeedAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                DELETE FROM [user-accounts]
+                WHERE [Email] = 'guest@pillmind.local'
+                  AND [PasswordHash] = 'preference-only-account';
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
